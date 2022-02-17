d957aed1154a138aaa6d4ae5bf8e38922c3c9307
iva2742@mail.ru <gial1719>
2022-02-17 15:40:08 +0300

    test commit

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -10,7 +10,7 @@ use yii\web\NotFoundHttpException;
 
 class CategoryController extends AppController
 {
-
+    //test commit
     public function actionView($id)
     {
         $category = Category::findOne($id);
