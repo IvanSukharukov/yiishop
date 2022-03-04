e52c12b630127bf65e0855a3f098c0a65c9b49d4
iva2742@mail.ru <gial1719>
2022-03-04 16:06:04 +0300

    10 - One product

--- models/Category.php
+++ models/Category.php
@@ -6,6 +6,16 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
+/**
+ * This is the model class for table "{{%category}}".
+ *
+ * @property integer $id
+ * @property integer $parent_id
+ * @property string $title
+ * @property string $description
+ * @property string $keywords
+ *
+ */
 class Category extends ActiveRecord
 {
     public static function tableName()
--- models/Product.php
+++ models/Product.php
@@ -6,10 +6,35 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
+/**
+ * This is the model class for table "{{%product}}".
+ *
+ * @property integer $id
+ * @property integer $category_id
+ * @property string $title
+ * @property string $content
+ * @property float $price
+ * @property float $old_price
+ * @property string $description
+ * @property string $keywords
+ * @property string $img
+ * @property boolean $is_offer
+ *
+ * @property string category app\models\Category
+ *
+ */
 class Product extends ActiveRecord
 {
     public static function tableName()
     {
         return 'product';
+    }
+
+
+
+    public function getCategory()
+    {
+        //id - Category, category_id = this (Product)
+        return $this->hasOne(Category::class, ['id' => 'category_id']);
     }
 }
